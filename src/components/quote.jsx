9e9f6d498a2f82fa61ac8c86a035ba4e9d2851a9
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-05-01 12:37:39 +0500

    fix linters

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import '../styles/quote.css';
 
 export default function quote() {
-    return (
-        <div className="quote-page-container">
-            Mathematics is not about numbers, equations, computations or algorithms;
-            it is about understanding. -William Paul Hurston
-        </div>
-    );
+  return (
+    <div className="quote-page-container">
+      Mathematics is not about numbers, equations, computations or algorithms;
+      it is about understanding. -William Paul Hurston
+    </div>
+  );
 }
